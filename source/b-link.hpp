c493d66e73112aa7d8f0b649e62e66caeec0bd99
XZegarra <33027320+XZegarra@users.noreply.github.com>
2021-01-26 09:53:10 -0500

    Add files via upload

---
+++ source/b-link.hpp
@@ -0,0 +1,367 @@
+// Copyright
+#ifndef SOURCE_B_LINK_HPP_
+#define SOURCE_B_LINK_HPP_
+
+#include <utility>
+#include<iostream>
+#include <queue>
+//const int B = 3;
+
+using namespace std;
+
+
+namespace EDA {
+namespace Concurrent {
+
+template <std::size_t B, typename Type>
+class BLinkTree {
+	class Node {
+	public:
+		bool isLeaf;						//IF IS LEAF, IT HAS TO BE CONNECTED WITH THE OTHER LEAFTS
+		int* key, size = 0;
+		Node** ptr;
+		Node() {
+			key = new int[B];
+			ptr = new Node * [B + 1];
+		};
+
+		void printValue() {
+			
+			if (key == nullptr) {
+				return;
+			}
+
+			for (int i = 0; i < size; i++) {
+				if (key[i] == NULL) {
+					break;
+				}
+				cout << key[i] << " ";
+			}
+			cout << endl;
+		}
+	};
+
+	Node* findParent(Node* current, Node* child) {					//WE WILL WALK TO THROW THE TREE TO FIND THE PARENT OF CHILD
+		Node* parent;
+
+		if (current->isLeaf || (current->ptr[0])->isLeaf) {			
+			return NULL;
+		}
+
+		for (int i = 0; i < current->size + 1; i++) {
+			if (current->ptr[i] == child) {
+				parent = current;
+				return parent;
+			}
+			else {
+				parent = findParent(current->ptr[i], child);
+				if (parent != NULL) {
+					return parent;
+				}
+			}
+		}
+		return parent;
+	}
+	void fitRoot() {
+		Node* current = root;
+		current = current->ptr[1];
+		if (!current) {
+			return;
+		}
+
+		while (!(current->isLeaf)) {
+			current = current->ptr[0];
+		}
+		if (current->key[0] > root->key[0]) {
+			Type temporal = (current->key[0]);
+			(root->key[0]) = current->key[0];
+			current->key[0] = temporal;
+		}
+	}
+	void insertInternal(int x, Node* current, Node* child) {
+		if (current->size < B) {
+			int i = 0;
+			while (x > current->key[i] && i < current->size) {
+				i++;
+			}
+
+			for (int j = current->size; j > i; j--) {
+				current->key[j] = current->key[j - 1];
+			}
+
+			for (int j = current->size + 1; j > i + 1; j--) {
+				current->ptr[j] = current->ptr[j - 1];
+			}
+
+			current->key[i] = x;
+			current->size++;
+			current->ptr[i + 1] = child;
+		}
+		else {
+			Node* newInternal = new Node;
+			int virtualKey[B + 1];
+			Node* virtualPtr[B + 2];
+
+			for (int i = 0; i < B; i++) {
+				virtualKey[i] = current->key[i];
+			}
+
+			for (int i = 0; i < B + 1; i++) {
+				virtualPtr[i] = current->ptr[i];
+			}
+
+			int i = 0, j;
+
+			while (x > virtualKey[i] && i < B) {
+				i++;
+			}
+
+			for (int j = B + 1; j > i; j--) {
+				virtualKey[j] = virtualKey[j - 1];
+			}
+
+			virtualKey[i] = x;
+
+			for (int j = B + 2; j > i + 1; j--) {
+				virtualPtr[j] = virtualPtr[j - 1];
+			}
+
+			virtualPtr[i + 1] = child;
+			newInternal->isLeaf = false;
+
+			current->size = (B + 1) / 2;
+
+			newInternal->size = B - ((B + 1) / 2);
+
+			for (int i = 0, j = current->size + 1; i < newInternal->size; i++, j++) {
+				newInternal->key[i] = virtualKey[j];
+			}
+
+			for (int i = 0, j = current->size + 1; i < newInternal->size + 1; i++, j++) {
+				newInternal->ptr[i] = virtualPtr[j];
+			}
+
+			if (current == root) {
+				Node* newRoot = new Node;
+
+				newRoot->key[0] = current->key[current->size];
+
+				newRoot->ptr[0] = current;
+				newRoot->ptr[1] = newInternal;
+				newRoot->isLeaf = false;
+				newRoot->size = 1;
+				root = newRoot;
+			}
+			else {
+				insertInternal(current->key[current->size], findParent(root, current), newInternal);
+			}
+		}
+	}
+
+ public:
+  typedef Type data_type;
+
+  BLinkTree() {
+	  root = NULL;
+  }
+
+  void destroy(Node* node) {
+	  if (node) {
+		  for (int i = 0; i < node->size; i++) {
+			  destroy(node->ptr[i]);
+		  }
+		  delete node;
+	  }
+  }
+
+  ~BLinkTree() {
+	  destroy(root);
+  }
+
+  std::size_t size() const {}
+
+  Node* getRoot() {
+	  return root;
+  }
+
+  bool empty() const {
+	  if (root == NULL) {
+		  return true;
+	  }
+	  return false;
+  }
+
+  bool search(const data_type& value) const {
+	  if (empty()) {								//IF THE TREE IS EMPTY WE WILL RETURN FALSE
+		  return false;
+	  }
+	  else {
+		  Node* current = root;
+		  
+		  while (current->isLeaf == false) {				//WHILE CURRENT IS NOT A LEAF WE WILL KEEP MOVING DOWN, WE WILL FIND THE NODE WHERE THE VALUE WE WANT TO FIND IS
+			  for (int i = 0; i < current->size; i++) {		//WE WILL MOVE THROW THE PTRs WETHER THE VALUE WE WANT TO FIND IS LESS OR MORE THAN THE CURRENT NODE
+				  for (int o = 0; o < current->size; o++) {				//ONCE WE ARE ON THE NODE WHERE OUR VALUE IS SUPPOSED TO BE. WE WILL LOOK FOR IT INSIDE THE KEYS OF THE NODE
+					  if (current->key[o] == value) {
+						  return true;
+					  }
+				  }
+
+				  if (value < current->key[i]) {
+					  current = current->ptr[i];
+					  break;
+				  }
+
+				  if (i == current->size - 1) {
+					  current = current->ptr[i + 1];
+					  break;
+				  }
+			  }
+		  }
+
+		  for (int i = 0; i < current->size; i++) {				//ONCE WE ARE ON THE NODE WHERE OUR VALUE IS SUPPOSED TO BE. WE WILL LOOK FOR IT INSIDE THE KEYS OF THE NODE
+			  if (current->key[i] == value) {
+				  return true;
+			  }
+		  }
+	  }
+	  return false;												//IF WE DON'T FIND THE VALUE IN THAT NODE WE WILL RETURN FALSE BECAUSE THE VALUE IS NOT THERE
+  }
+
+  
+
+  void insert(const data_type& value) {
+	  if (root == NULL) {				//WE CHECK IF THE VALUE THAT WE WANT TO INSERT IS THE FIRST ONE THAT THE TREE IS RECEIVING
+		  root = new Node;
+		  root->key[0] = value,
+		  root->isLeaf = true;
+		  root->size = 1;
+	  }
+	  else {
+		  Node* current = root;		//CURRENT WILL BE ROOT
+		  Node* parent;				
+		  
+		  while (current->isLeaf == false) {		//WE WILL GO ALL THE WAY DOWN UNTIL FIND A LEAF
+			  parent = current;						//PARENT WILL BE CURRENT IF CURRENT IT'S NOT A LEAF
+			  for (int i = 0; i < current->size; i++) { //WE CHECK THE CURRENT NODE
+				  if (value < current->key[i]) {		//IF WE WANT TO INSERT A NODE LESS THAN THE CURRENT KEY WE WILL GO TO THE LEFT NODE
+					  current = current->ptr[i];		
+					  break;
+				  }
+				  if (i == current->size - 1) {			//IF WE ARRIVE TO THE FINAL PTR WE WILL GO TO THE RIGHTEST PTR OF THE CURRENT NODE
+					  current = current->ptr[i + 1];
+					  break;
+				  }
+			  }
+		  }
+
+		  if (current->size < B) {				//IF WE DO NOT HAVE OVERFLOW
+			  int leftIndex = 0;
+			  while (value > current->key[leftIndex] && leftIndex < current->size) {		//WHILE THE VALUE IT'S GREATER THAN THE ACTUAL KEY AND LESS THAN THE CURRENT SIZE, WE WILL ADD 1 TO IT
+				  leftIndex++;
+			  }
+			  for (int rightIndex = current->size; rightIndex > leftIndex; rightIndex--) { //WHILE THE VALUE IS LESS THAN THE KEYS FROM THE RIGHT
+				  current->key[rightIndex] = current->key[rightIndex - 1];	//WE BEGIN SWAPPING
+			  }
+			  current->key[leftIndex] = value;	//WE FINISH THE SWAP
+			  current->size++;	//WE FIX THE SIZE
+
+			  current->ptr[current->size] = current->ptr[current->size - 1];	//WE FIX THE PTRs
+			  current->ptr[current->size - 1] = NULL;
+		  }
+		  else {								//IF WE HAVE OVERFLOW
+			  Node* newLeaf = new Node;
+
+			  int temporalNode[B + 1];
+
+			  for (int i = 0; i < B; i++) {			//WE COPY THE WHOLE CURRENT NODE IN OUR TEMPORAL
+				  temporalNode[i] = current->key[i];
+			  }
+
+			  int leftIndex = 0, rightIndex;
+
+			  while (value > temporalNode[leftIndex] && leftIndex < B) {	//WE GET THE LEFT INDEX
+				  leftIndex++;
+			  }
+
+			  for (rightIndex = B + 1; rightIndex > leftIndex; rightIndex--) {
+				  temporalNode[rightIndex] = temporalNode[rightIndex - 1];	//WE BEGIN TO OPEN SPACE FOR THE NEW KEY
+			  }
+
+			  temporalNode[leftIndex] = value;	//WE INSERT THE VALUE ON THE SPACE
+			  newLeaf->isLeaf = true;
+
+			  current->size = (B + 1) / 2;		//WE BEGIN THE SPLITTING INDICATING THE NEW SIZES
+			  newLeaf->size = B + 1 - ((B + 1) / 2);
+
+			  current->ptr[current->size] = newLeaf;			//WE FIX THE PTRs
+																				
+			  newLeaf->ptr[newLeaf->size] = current->ptr[B];
+
+			  current->ptr[B] = NULL;
+
+			  for (leftIndex = 0; leftIndex < current->size; leftIndex++) { //WE COPPY OUR KEYS FROM OUR TEMPORAL NODE TO CURRENT FROM LEFT TO RIGHT
+				  current->key[leftIndex] = temporalNode[leftIndex];
+			  }
+			  
+			  for (leftIndex = 0, rightIndex = current->size; leftIndex < newLeaf->size; leftIndex++, rightIndex++) { //WE COPPY OUR KEYS FROM OUR TEMPORAL NODE TO NEWLEAF FROM RIGHT TO LEFT
+				  newLeaf->key[leftIndex] = temporalNode[rightIndex];
+			  }
+
+			  if (current == root) {				//IF CURRENT IS ROOT WE WILL HAVE TO CREATE A NEW ONE AND SET ALL THE VALUES
+				  Node* newRoot = new Node;
+
+				  newRoot->key[0] = newLeaf->key[0];
+				  newRoot->ptr[0] = current;
+				  newRoot->ptr[1] = newLeaf;
+				  newRoot->isLeaf = false;
+				  newRoot->size = 1;
+				  root = newRoot;
+			  }
+			  else {							//IF NOT WE ARE GONNA HAVE TO DO THE INTERAL INSERT
+				  insertInternal(newLeaf->key[0], parent, newLeaf);
+			  }
+		  }
+	  }
+	  //fitRoot();
+  }
+
+  void remove(const data_type& value) {}
+
+  void printLeafs() {
+	  Node* current = root;
+
+	  while (!(current->isLeaf)) {
+		  current = current->ptr[0];
+	  }
+	  while (current) {
+		  current->printValue();
+		  current = current->ptr[B-1];
+		  if (!(current)) {
+			  break;
+		  }
+	  }
+  }
+
+  void amplitud() {
+	  cout << "RECORRIDO POR AMPLITUD:" << endl;
+	  cout << "RAIZ: ";
+	  root->printValue();
+	  cout << "HIJO IZQUIERDO: ";
+	  root->ptr[0]->printValue();
+	  cout << "HIJO DERECHO: ";
+	  root->ptr[1]->printValue();
+
+	  cout << "HOJAS: "; 
+	  printLeafs();
+  }
+
+ private:
+  data_type* data_;
+  Node* root;
+
+};
+
+}  // namespace Concurrent
+}  // namespace EDA
+
+#endif  // SOURCE_B_LINK_HPP_
---
+++ source/main.cpp
@@ -0,0 +1,107 @@
+// Copyright 2021 Roger Peralta Aranibar
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <thread>
+
+#include "b-link.hpp"
+
+std::size_t NUMBER_THREADS = 8;
+
+const std::size_t NUMBER_OPERATIONS = 2;
+
+int MIN_VALUE = 1;
+int MAX_VALUE = 15000;
+
+std::random_device rd;
+
+class TreeSearcher {
+ public:
+  TreeSearcher(unsigned int id, EDA::Concurrent::BLinkTree<3, int> *b_link)
+      : id_(id), b_link_(b_link) {}
+
+  void operator()() {
+    std::uniform_int_distribution<int> distribution(MIN_VALUE, MAX_VALUE);
+    for (std::size_t i = 0; i < NUMBER_OPERATIONS; ++i) {
+      std::cout << distribution(rd) << "\n";
+      b_link_->search(distribution(rd));
+    }
+  }
+
+ private:
+  unsigned int id_;
+  EDA::Concurrent::BLinkTree<3, int> *b_link_;
+};
+
+void sequential_insert(EDA::Concurrent::BLinkTree<3, int> *b_link) {
+  std::uniform_int_distribution<int> distribution(MIN_VALUE, MAX_VALUE);
+  for (std::size_t i = 0; i < NUMBER_OPERATIONS; ++i) {
+    std::cout << distribution(rd) << "\n";
+     b_link->insert(distribution(rd));
+  }
+}
+
+void run_test() {
+  EDA::Concurrent::BLinkTree<3, int> b_link;
+
+  sequential_insert(&b_link);
+
+  std::thread *threads = new std::thread[NUMBER_THREADS];
+
+  for (std::size_t i = 0; i < NUMBER_THREADS; ++i) {
+    threads[i] = std::thread(TreeSearcher(i, &b_link));
+  }
+  for (std::size_t i = 0; i < NUMBER_THREADS; ++i) {
+    threads[i].join();
+  }
+
+  b_link.amplitud();
+}
+
+void run_test2() {
+	EDA::Concurrent::BLinkTree<3, int> b_link;
+
+	b_link.insert(1);
+	b_link.insert(4);
+	b_link.insert(7);
+	b_link.insert(10);
+	b_link.insert(17);
+	b_link.insert(21);
+	b_link.insert(31);
+	b_link.insert(25);
+	b_link.insert(19);
+	b_link.insert(20);
+	b_link.insert(28);
+	b_link.insert(42);
+
+
+	//b_link.insert(6);
+	//b_link.insert(16);
+	//b_link.insert(26);
+	//b_link.insert(36);
+	//b_link.insert(46);
+
+	//b_link.amplitud();
+
+	cout << "\t"; b_link.getRoot()->printValue();
+	cout << "\t\t";b_link.getRoot()->ptr[1]->printValue();
+	cout << "\t";b_link.getRoot()->ptr[1]->ptr[0]->printValue();
+	cout << "\t";b_link.getRoot()->ptr[1]->ptr[1]->printValue();
+	cout << "\t";b_link.getRoot()->ptr[0]->ptr[2]->printValue();
+	cout << "\t";b_link.getRoot()->ptr[0]->ptr[3]->printValue();
+	cout << b_link.search(20);
+	//cout << "\t\t\t"; b_link.getRoot()->printValue();
+	//cout << "\t\t";b_link.getRoot()->ptr[0]->printValue();
+	//cout << "\t";b_link.getRoot()->ptr[1]->ptr[0]->printValue();
+	//cout << "\t";b_link.getRoot()->ptr[0]->ptr[1]->printValue();
+	//cout << "\t";b_link.getRoot()->ptr[0]->ptr[2]->printValue();
+
+
+	//b_link.printLeafs();
+}
+
+int main() {
+  run_test2();
+  return 0;
+}
